b77107b4aaa6ec84bdaa21ec5ff67b20437bc2c4
Angel <figueroa10@newpaltz.edu>
2019-09-23 12:16:54 -0400

    Sept 23 class

--- server/index.js
+++ server/index.js
@@ -1,8 +1,14 @@
 const express = require('express');
+const path = require('path'); //var path
 const app = express();
 const port = 3000;
 
 app
+    //middleware
+    .use(   '/static', 
+            express.static(path.join(__dirname, '../NoFramework')))
+    //must reference path variable
+
     .get(   '/',
             (req, res) => res.send('Hello World!!!')
     )
