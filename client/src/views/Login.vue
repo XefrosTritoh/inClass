e1605444f1494e69ddf240aa12744731c4225119
Angel <figueroa10@newpaltz.edu>
2019-11-21 11:14:12 -0500

    added error checking for names (Nov 18)

--- client/src/views/Login.vue
+++ client/src/views/Login.vue
@@ -11,37 +11,29 @@
                 <p class="panel-heading">
                     Login
                 </p>
+                <div class="panel-block">
 
-
-    <div class="field">
-            <div class="control has-icons-left has-icons-right">
-                 <input v-model="name" class="input" type="text" placeholder="Name...">
-
-                 <span class="icon is-small is-left">
-                    <i class="fas fa-user"></i>
-                </span>
-
-                <span class="icon is-small is-right">
-                     <i class="fas fa-exclamation-triangle"></i>
-                </span>
-            </div>
-            <p class="help is-danger">{{error}}</p>
-    </div>
-
-
-
-
-
-                <div class= "panel-block">
-                    <div class="field has-addons">
-                        <div class="control is-expanded">
-                        </div>
-                        <div class="control">
-                            <a class="button is-info" @click.prevent="join">
+                    <div class="field" :class="{ 'is-danger': error }">
+                        <div class="field has-addons">
+                            <div class="control has-icons-left has-icons-right">
+                                <input v-model="name" class="input" type="text" placeholder="Your Name">
+                                
+                                <span class="icon is-small is-left">
+                                <i class="fas fa-user"></i>
+                                </span>
+                                <span class="icon is-small is-right">
+                                <i class="fas fa-exclamation-triangle"></i>
+                                </span>
+                            </div>
+                            <div class="control">
+                                <a class="button is-info" @click.prevent="join">
                                 Login
-                            </a>
+                                </a>
+                            </div>
                         </div>
+                        <p class="help is-danger">{{error}}</p>
                     </div>
+                    
                 </div>
             </ul>
         </div>
@@ -60,31 +52,27 @@ export default {
     methods: {
         join(){
             Game_Server.Join(this.name)
-            .then(x=> this.$router.push({name: 'game'})
-            .catch(err=>{
-                console.error(err)
-                this.error = err.message;
-            })
+                .then(x=> this.$router.push( { name: 'game'} ) )
+                .catch(err=> {
+                    console.error(err);
+                    this.error = err.message;
+                });
         }
     }
-
 }
 </script>
 
 <style lang="scss">
-
-    .fas.fa-exclamation-triangle{
+    .fas.fa-exclamation-triangle {
         display: none;
     }
-
-    .is-danger{
-        .fa-exclamation-triangle{
-        display: inline;
-        color: red;
+    .is-danger {
+        .fa-exclamation-triangle {
+            display: inline;
+            color: red;
         }
-        .input{
+        .input {
             border-color: red;
         }
     }
-    
 </style>
