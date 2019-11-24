2b9dc04642dab63470f1ed80de262d777aca58a5
Angel <figueroa10@newpaltz.edu>
2019-11-24 14:49:08 -0500

    Nov 21 class

--- server/index.js
+++ server/index.js
@@ -2,19 +2,25 @@ const express = require('express');
 const path = require('path'); //var path
 const userController = require('./controllers/users');
 const gameController = require('./controllers/game');
-
+const { CustomError } = require('./models/Custom_Error');
 const app = express(); //must be executed first
 
 const port = process.env.PORT || 3000;
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept" , "Authorization");
     next();
 });
 
-//match paths to functions( ex: /static, express fucntion)
-//middleware
+app.use(function(req, res, next) {
+    const arr = req.headers.authorization.split(" ")
+    if(arr.length > 1 && +arr[1]){
+        req.user_id = +arr[1];
+    }else{
+        next(new CustomError(403, "Please login"));
+    }
+})
 app
     .use(express.json())
     .get('/port' , (req,res) => res.send("Using Port: " + port))
